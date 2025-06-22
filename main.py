01af5b7092822e07c0f982b5fa82e848202500bc
Thiagarajan Baskarasubramanian <itthiagu@gmail.com>
2025-06-22 15:02:43 +0530

    Update main.py
    Heading Change

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ tweet_chain = tweet_prompt | gemini_model
 
 import streamlit as st
 
-st.header("Tweet Generator - SATVIK")
+st.header("Smart Tweet Generator")
 
 st.subheader("Generate tweets using Generative AI")
 
